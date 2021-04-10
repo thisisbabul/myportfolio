7a9adcde554e59c71cd99796ef5eb1d588ff013f
babul145 <babul.cu.cse35@gmail.com>
2021-04-10 12:53:36 +0600

    first commit

---
+++ src/main/java/com/iambabul/portfolio/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.iambabul.portfolio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to spring boot project";
+    }
+}
